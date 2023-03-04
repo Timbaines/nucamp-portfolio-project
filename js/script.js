24df2cf46e7e6c4b7053c964f6b9cbf0948f9040
Tim Baines <timbaines@timbaines.com>
2023-03-04 02:52:37 -0500

    commit for presentation

--- js/script.js
+++ js/script.js
@@ -68,36 +68,4 @@ $(document).ready(function () {
       ".typewrite > .wrap { border-right: 0.08em solid transparent}";
     document.body.appendChild(css);
   };
-
-  ///////////////////////////////////////////////////////////
-  // Search Bar
-
-  const searchFocus = document.getElementById("search-focus");
-  const keys = [
-    { keyCode: "AltLeft", isTriggered: false },
-    { keyCode: "ControlLeft", isTriggered: false },
-  ];
-
-  window.addEventListener("keydown", (e) => {
-    keys.forEach((obj) => {
-      if (obj.keyCode === e.code) {
-        obj.isTriggered = true;
-      }
-    });
-
-    const shortcutTriggered =
-      keys.filter((obj) => obj.isTriggered).length === keys.length;
-
-    if (shortcutTriggered) {
-      searchFocus.focus();
-    }
-  });
-
-  window.addEventListener("keyup", (e) => {
-    keys.forEach((obj) => {
-      if (obj.keyCode === e.code) {
-        obj.isTriggered = false;
-      }
-    });
-  });
 });
